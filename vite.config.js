46cdb080eca5a2399b9b633ab1e6a4ede7a5a528
Bhagyashri Patil <bhagyashripatil8767@gmail.com>
2025-10-03 19:05:03 +0530

    fix vite base path for vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Replace with your repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/bhagyashri-portfolio/',
-});
+  base: './',   // 👈 हे नक्की ठेवा
+})
